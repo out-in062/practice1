516072fc241172b6ae0ffb82b2ab3b20504dd248
tonouchi <y.tonouchi.sys23@morijyobi.ac.jp>
2023-10-05 11:03:04 +0900

    「pra2.javaに出力処理追記」

--- pra2.java
+++ pra2.java
@@ -1,1 +1,7 @@
-Hello world
+[200~public class HelloWorld {
+	    public static void main(String[] args) {
+		            for (int i = 0; i < 100; i++) {
+				                System.out.println("Hello World");
+						        }
+			        }
+}
